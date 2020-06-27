d6be6a8635316055f2c430fb9564e150e0bd4ece
Maria P. Campos <mpfc.maria@gmail.com>
2020-06-27 16:23:14 -0300

    adding checked function

--- app.js
+++ app.js
@@ -47,9 +47,16 @@ function deleteCheck(e){
     //DELETE TODO
     if(item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add("fall")
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
-
-
+    //CHECK MARK
+    if(item.classList[0] === "complete-btn"){
+        const todo = item.parentElement
+        todo.classList.toggle("completed");
+    }
 }
